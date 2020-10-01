56635ecd3c4ef3f29c689744bd8075072d7cbb2e
marshingjay <jam605@gmail.com>
2020-10-01 10:30:15 -0500

    fixed value generation bug when running without genetic algo

--- back_testing/v2/strategy/indicators/bollinger_bands.py
+++ back_testing/v2/strategy/indicators/bollinger_bands.py
@@ -8,7 +8,7 @@ class BollingerBands(Indicator):
     def genData(self, dataset, gen_new_values=True, value='close'):
         period = findParams(self.params, ['period'])[0]
         
-        boll_sma = SMA([period], _name='boll_sma')
+        boll_sma = SMA([period], gen_new_values=gen_new_values, _name='boll_sma')
         boll_sma.genData(dataset, value=value)
         dataset['boll_stdev'] = dataset[value].rolling(int(period.value)).std()
         dataset['boll_upper'] = dataset['boll_sma'] + (dataset['boll_stdev'] * 2)
--- back_testing/v2/strategy/indicators/macd.py
+++ back_testing/v2/strategy/indicators/macd.py
@@ -8,17 +8,17 @@ class MACD(Indicator):
     
 
     def genData(self, dataset, gen_new_values=True, value='close'):
-        # due to the nature of macd we will always generate new values
-        # if this causes a problem we can change it 
+        
 
         #params named 'ema_slow', 'ema_fast', and 'signal' must all be present
         ema_slow_param, ema_fast_param, signal_param = findParams(self.params, ['ema_slow', 'ema_fast', 'signal'])
-        ema_slow_value = ema_slow_param.genValue()
-        ema_fast_param.up = ema_slow_value
-        ema_fast_value = ema_fast_param.genValue()
-        signal_param.up = ema_slow_value
-        signal_param.low = ema_fast_value
-        signal_param.genValue()
+        if gen_new_values:
+            ema_slow_value = ema_slow_param.genValue()
+            ema_fast_param.up = ema_slow_value
+            ema_fast_value = ema_fast_param.genValue()
+            signal_param.up = ema_slow_value
+            signal_param.low = ema_fast_value
+            signal_param.genValue()
 
         # generate signal dataset (ema)
         ema_slow = EMA([ema_slow_param], _name='ema_slow')
