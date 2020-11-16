03eaa0ea5a2c6747328fcacc5d1ee4231205926d
marshingjay <jam605@gmail.com>
2020-11-16 16:12:53 -0600

    commented Param init

--- back_testing/v2/strategy/indicators/param.py
+++ back_testing/v2/strategy/indicators/param.py
@@ -1,6 +1,35 @@
+'''
+FILE: param.py
+AUTHORS:
+    -> Jacob Marshall (marshingjay@gmail.com)
+WHAT:
+    -> This file contains the Param superclass
+'''
+
 import random
 
+'''
+CLASS: Param
+WHAT:
+    -> Params store values that exist as a part of strategies/indicators
+    -> These values exist as part of calculations
+    -> It's possible we'd want to change/randomize these values over a given range for optimization purposes
+    -> This class allows for that functionality
+'''
 class Param:
+
+    '''
+    ARGS:
+        -> _low (Float): lower bound of range of values that Param can take on
+        -> _high (Float): upper bound of range of values that Param can take on
+        -> _prec (Float): precision of value
+        -> _default (Float): initial value for this param
+        -> _name (String): the name of this param
+    RETURN:
+        -> None
+    WHAT: 
+        -> Initializes param value, range, precision, and name
+    '''
     def __init__(self, _low, _up, _prec, _name, _default):
         self.low = _low
         self.up = _up
