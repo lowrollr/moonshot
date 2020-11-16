e60c757dfbb2a78810cf8ccb9e7c7cd3d9e6ebc4
marshingjay <jam605@gmail.com>
2020-11-16 17:00:49 -0600

    commented strategy init

--- back_testing/v2/strategy/strategies/strategy.py
+++ back_testing/v2/strategy/strategies/strategy.py
@@ -1,7 +1,32 @@
+'''
+FILE: strategy.py
+AUTHORS:
+    -> Jacob Marshall (marshingjay@gmail.com)
+WHAT:
+    -> This file contains the Strategy superclass
+'''
+
+'''
+CLASS: Strategy
+WHAT:
+    -> Implements a given order execution strategy
+'''
 class Strategy:
+
+    '''
+    ARGS:
+        -> None
+    RETURN:
+        -> None
+    WHAT: 
+        -> default __init__ implementation
+        -> Nothing to see here, every subclass will have its own implementation of this
+    '''
     def __init__(self):
         self.indicators = []
         self.is_ml = False
+
+        
     def process(self, data):
         # this happens every tick
         # use this to keep track of things that need to be kept track of over multiple ticks
