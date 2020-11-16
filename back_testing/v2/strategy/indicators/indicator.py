2531b22a9699a3a12b137e653d7467df26d0e9c6
marshingjay <jam605@gmail.com>
2020-11-16 16:46:13 -0600

    commented genData

--- back_testing/v2/strategy/indicators/indicator.py
+++ back_testing/v2/strategy/indicators/indicator.py
@@ -36,10 +36,25 @@ class Indicator:
             self.name = self.__class__.__name__ + _appended_name
         self.best_values = {}
 
+
+    '''
+    ARGS:
+        -> dataset (DataFrame): dataset to add the indicator values as a column to
+        -> gen_new_values (Boolean) <optional>: weather or not we should generate new values for each param belonging
+            to this Indicator
+        -> value (String) <optional>: dataframe column name to use for calculations
+    RETURN:
+        -> None
+    WHAT: 
+        -> The default implementation doesn't actually do anything with the dataset, but subclass implementations should/do
+        -> The default implementation is used to store and vary a value used within a strategy that does not need to be added
+            to the dataset or have an abstracted implementation
+        -> Here, instead of adding a column to the dataframe, we simply generate new values for each param if necessary
+    '''
     def genData(self, dataset, gen_new_values=True, value='close'):
         # generic indicators don't get added to the dataset as columns because they are simply
         # data used within strategies and thus don't need to be applied to data frames
-        if gen_new_values:
+        if gen_new_values: # if we are generating new values, call genValue for each param belonging to this indciator
             for x in self.params:
                 x.genValue()
     
