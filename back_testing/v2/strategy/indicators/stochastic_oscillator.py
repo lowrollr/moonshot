bfc8358ba619461dc4fd8008cbfbbdfbeffa7a21
Ross <rhcopeland101@gmail.com>
2020-11-19 15:41:03 -0600

    adding stochastic oscillator indicator

--- back_testing/v2/strategy/indicators/stochastic_oscillator.py
+++ back_testing/v2/strategy/indicators/stochastic_oscillator.py
@@ -1,10 +1,39 @@
+'''
+FILE: ema.py
+AUTHORS:
+    -> Jacob Marshall (marshingjay@gmail.com)
+WHAT:
+    -> This file contains the Stochastic Oscillator Indicator
+'''
+
 from v2.strategy.indicators.indicator import Indicator
 from v2.utils import findParams
 from v2.strategy.indicators.sma import SMA
 import pandas
 
+'''
+CLASS: Variance
+WHAT:
+    -> Calculates the stocastic oscillation and adds the approprite column to the dataset
+    -> What is stochastic oscillator? --> https://www.investopedia.com/terms/s/stochasticoscillator.asp
+    -> Params Required:
+        -> 'highlow_range'
+        -> 'k_period'
+'''
+
 class StochasticOscillator(Indicator):
     
+    '''
+    ARGS:
+        -> dataset (DataFrame): dataset to add the indicator values as a column to
+        -> gen_new_values (Boolean) <optional>: weather or not we should generate new values for each param belonging
+            to this Indicator
+        -> value (String) <optional>: dataframe column name to use for calculations
+    RETURN:
+        -> None
+    WHAT: 
+        -> calculates and adds the stochastic oscillation of the specified value over the given period to the dataset
+    '''
     def genData(self, dataset, gen_new_values=True, value='close'):
         param_highlow_range, param_k_period_sma = findParams(self.params, ['highlow_range', 'k_period'])
         if gen_new_values:
@@ -17,6 +46,3 @@ class StochasticOscillator(Indicator):
 
         k_period_sma = SMA([param_k_period_sma], _name='stosc_d')
         k_period_sma.genData(dataset, gen_new_values=gen_new_values, value='stosc_k')
-
-        
-        
