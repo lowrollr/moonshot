77a5e762a9d958d64330c32c9dbd2667748b558c
Ross <rhcopeland101@gmail.com>
2020-12-21 19:29:19 -0600

    now using quick soort instead of bubble

--- back_testing/reports/script.js
+++ back_testing/reports/script.js
@@ -1,9 +1,5 @@
 function sortRows(n) {
     let table = document.getElementById("ind_movements_data");
-    let switching = true;
-    let dir = "asc";
-    var shouldSwitch;
-    let switchcount = 0;
 
     let header = table.rows[0];
     let final_arr;
@@ -14,120 +10,62 @@ function sortRows(n) {
         table_arr.push([id, table.rows[i]]);
     }
     let rows = table_arr;
-    while( switching ) {
-        
-        switching = false;
-        for (i = 0; i < rows.length-2; i++) {
-            shouldSwitch = false;
-            let x = rows[i][0]
-            let y = rows[i + 1][0]
 
-            if ( dir === "asc" ) {
-                if (x > y) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (x < y) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-        if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-            rows = swap(rows, i+1, i);
-            //rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            // Each time a switch is done, increase this count by 1:
-            switchcount ++;
-          } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
-            if (switchcount == 0 && dir == "asc") {
-              dir = "desc";
-              switching = true;
-            }
-        }
-        final_arr = rows;
+    if ( this.direction === undefined ) {
+        this.direction = true;
     }
+    final_arr = quickSort(rows, 0, rows.length - 1);
     table.innerHTML = "";
     table.appendChild(header);
+
+    if ( this.direction === false) {
+        final_arr = final_arr.reverse();
+    }
 
     for( let i = 0; i < final_arr.length; i++) {
         table.appendChild(final_arr[i][1]);
     }
+
+    this.direction = this.direction === true ? false : true;
+    console.log(this.direction);
 }
 
-function quickSort(arr, left, right) {
-	let index;
-
-	//We want to check if we even need to do the sorting
-	if (arr.length > 1)
-	{
-		//We first sort the function, and return the center which is used to divide the array
-		index = sortingUsingPivot(arr, left, right);
-
-		//If there are more elements on the left side of pivot that needs to be sorted
-		if (left < index - 1)
-		{
-			//will put the entire left of the array into the quicksort again
-			quickSort(arr, left, index - 1);
-		}
-
-		//If there are more elements on the right side of pivot that needs to be sorted
-		if (index < right) 
-		{
-			//will put the entire right of the array into the quicksort again
-			quickSort(arr, index, right);
-		}
-	}
-} 
-
-function sortingUsingPivot(arr, left, right) {
-	//We are using the middle element of the array as our pivot
-	let pivot = arr[Math.floor((right + left) / 2)];
-	let l = left; //This is keeping track of left pointer
-	let r = right; //this is keeping track of right pointer
-
-	//Keep going until left pointer passes the right pointer
-	while (l <= r) 
-	{
-		//Used Find the first element on the left side that is larger than the pivot element.
-		//So that we know this is the element we want to move to the other side 
-		while (arr[l] < pivot)
-		{
-			//Keep searching until we pass the pivot
-			l++;
-		}
-
-		//Used Find the first element on the right side that is smaller than the pivot element.
-		//So that we know this is the element we want to move to the other side 
-		while (arr[r] > pivot) 
-		{
-			//Keep searching until we pass the pivot
-			r--;
-		} 
-
-		//we want to swap the two elements as long as left pointer doesn't pass the right pointer
-		if (l <= r) {
-			swap(arr, l, r);
-			l++;
-			r--;
-		}
-	}
-
-	//Return the left pointer as that is our new center to divide the array
-	return l;
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
 }
 
-function swap(arr, leftIndex, rightIndex) {
-	//We basically just swap the two items from the two different index
-	let temp = arr[leftIndex];
-	arr[leftIndex] = arr[rightIndex];
-    arr[rightIndex] = temp;
-    return arr;
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)][0], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i][0] < pivot) {
+            i++;
+        }
+        while (items[j][0] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
 }
