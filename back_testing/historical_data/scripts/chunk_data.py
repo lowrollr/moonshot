b172b10724b2749a0bb267e51de5c52e9380bcb9
marshingjay <jam605@gmail.com>
2021-01-19 18:18:31 -0600

    updated chunk data

--- back_testing/historical_data/scripts/chunk_data.py
+++ back_testing/historical_data/scripts/chunk_data.py
@@ -3,12 +3,12 @@ import numpy as np
 import os
 import sys
 
-
-filename = 'ETHUSDT-1m-data.csv'
+coin = sys.argv[1].upper()
+filename = f'{coin}USDT-1m-data.csv'
 timestamp_label = 'close_time'
 good_amount = 64800
 
-data = pd.read_csv(filename)
+data = pd.read_csv(f'../binance/{filename}')
 data.reset_index(inplace=True)
 data['diff'] = data[timestamp_label].diff()
 data['diff'] = data['diff'].shift(1)
@@ -20,6 +20,6 @@ for x in gaps:
     chunk_size = x - cur_chunk_start
     if chunk_size > good_amount:
         chunk = data.iloc[cur_chunk_start:x+1][['close_time', 'open', 'high', 'low', 'close', 'volume']]
-        chunk.to_csv(filename[:-4] + '_chunk' + '0'*(6 -len(str(chunk_num))) + str(chunk_num) + '.csv')
+        chunk.to_csv(f'../binance/{coin}/1m/{coin}USDT-1m-data_chunk' + '0'*(6 -len(str(chunk_num))) + str(chunk_num) + '.csv')
         chunk_num += 1
     cur_chunk_start = x + 2
