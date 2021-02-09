83f1c52620541df1f08aad11e33a6673ba404bac
marshingjay <jam605@gmail.com>
2021-02-08 21:51:21 -0600

    implemented manual candles

--- main_data_consumer/src/data_consumer.go
+++ main_data_consumer/src/data_consumer.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"math"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -18,6 +20,7 @@ type DataConsumer struct {
 	NumConnections int
 	Clients        map[string]*Client
 	Coins          *[]string
+	Candlesticks   map[string]*Candlestick
 }
 
 func (data *DataConsumer) InitializeServer(wg *sync.WaitGroup) {
@@ -104,11 +107,12 @@ func (data *DataConsumer) StartConsume() {
 
 func (data *DataConsumer) Consume() {
 	InitConsume()
-
+	data.Candlesticks = make(map[string]*Candlestick)
 	klineInterval := "1m"
 	log.Println("Start Consuming")
 
 	for _, symbol := range *data.Coins {
+		data.Candlesticks[symbol] = nil
 		symbol = strings.ToLower(symbol) + "usdt"
 		go data.KlineGoRoutine(symbol, klineInterval)
 	}
@@ -134,9 +138,52 @@ func (data *DataConsumer) KlineGoRoutine(symbol string, klineInterval string) {
 }
 
 func (data *DataConsumer) KlineDataConsumerStoreSend(event *binance.WsTradeEvent) {
-	now := time.Now()
-	times_per_min := 1
-
+	now := int32(math.Trunc(float64(time.Now().UnixNano()) / float64(time.Minute.Nanoseconds())))
+	trade_price_fl64, _ := strconv.ParseFloat(event.Price, 32)
+	trade_price := float32(trade_price_fl64)
+	trade_coin := event.Symbol[:len(event.Symbol)-4]
+	candle := data.Candlesticks[trade_coin]
+	if candle == nil {
+		data.Candlesticks[trade_coin] = &Candlestick{
+			coin:      trade_coin,
+			startTime: now,
+			open:      trade_price,
+			high:      trade_price,
+			low:       trade_price,
+			close:     trade_price}
+	} else if candle.startTime != now {
+		wg := new(sync.WaitGroup)
+		wg.Add(len(data.Clients))
+		for destinationStr, client := range data.Clients {
+			klineMessage := SocketKlineMessage{
+				Source:      "main_data_consumer",
+				Destination: destinationStr,
+				Msg:         *data.Candlesticks[trade_coin],
+			}
+			if destinationStr == "frontend" {
+				log.Println(klineMessage)
+			}
+			klineByte, _ := json.Marshal(klineMessage)
+			client.WriteSocketMessage(klineByte, wg)
+		}
+		wg.Wait()
+		log.Println(event)
+		data.Candlesticks[trade_coin] = &Candlestick{
+			coin:      trade_coin,
+			startTime: now,
+			open:      trade_price,
+			high:      trade_price,
+			low:       trade_price,
+			close:     trade_price}
+	} else {
+		candle.close = trade_price
+		if trade_price > candle.high {
+			candle.high = trade_price
+		}
+		if trade_price < candle.low {
+			candle.low = trade_price
+		}
+	}
 	//store in db
 	// err := Dumbo.StoreCryptoKline(event)
 	// if err != nil {
@@ -144,23 +191,7 @@ func (data *DataConsumer) KlineDataConsumerStoreSend(event *binance.WsTradeEvent
 	// 	printNumSockets()
 	// }
 
-	// wg := new(sync.WaitGroup)
-	// wg.Add(len(data.Clients))
-	// for destinationStr, client := range data.Clients {
-	// 	klineMessage := SocketKlineMessage{
-	// 		Source:      "main_data_consumer",
-	// 		Destination: destinationStr,
-	// 		Msg:         event.Kline,
-	// 	}
-	// 	if destinationStr == "frontend" {
-	// 		log.Println(klineMessage)
-	// 	}
-	// 	klineByte, _ := json.Marshal(klineMessage)
-	// 	client.WriteSocketMessage(klineByte, wg)
-	// }
-	// wg.Wait()
-	log.Println(event)
-	EfficientSleep(times_per_min, now, time.Second)
+	// EfficientSleep(times_per_min, now, time.Second)
 }
 
 func InitConsume() {
