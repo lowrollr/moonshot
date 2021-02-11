9261e3abf4a11f9d5bb76c59c5a993c5b8b82d36
Ross <rhcopeland101@gmail.com>
2021-02-11 09:57:11 -0600

    wrong spelling of var in vars.py

--- beverly_hills/src/bh.py
+++ beverly_hills/src/bh.py
@@ -46,13 +46,13 @@ class BeverlyHills():
         coins = ""
         while True:
             #change this to something else 
-            rawMsg = {'msg':'', 'src':vars.containerToId['beverly_hills'], 'dest':vars.containerToId['main_data_consumer']}
+            rawMsg = {'msg':'', 'src':vars.containersToId['beverly_hills'], 'dest':vars.containersToId['main_data_consumer']}
             bytesMsg = client_file.constructMsg(json.dumps(rawMsg), "coinRequest")
             conn.sendall(bytes(bytesMsg, encoding='utf-8'))
             coins = client_file.readData(conn)
             if len(coins) > 0:
                 break
-        print("Received coins from data coinsumer")
+        print("Received coins from data consumer")
         self.coins = coins
 
     def startServer(self):
@@ -103,7 +103,7 @@ class BeverlyHills():
         s.close()
 
         #send start to data consumer
-        rawMessage = {'msg':'', 'src':vars.containerToId['beverly_hiills'], 'dest':vars.containerToId['main_data_consumer']}
+        rawMessage = {'msg':'', 'src':vars.containersToId['beverly_hiills'], 'dest':vars.containersToId['main_data_consumer']}
         bytesMessage = client_file.constructMsg(json.dumps(rawMessage), "start")
         self.consumerSocket.sendall(bytes(bytesMessage,encoding='utf-8'))
         while True:
