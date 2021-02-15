13bddcbef695f718c47626f798df3c2c4788e6fe
marshingjay <jam605@gmail.com>
2021-02-15 10:44:21 -0600

    implemented SMA

--- portfolio_manager/src/indicators.go
+++ portfolio_manager/src/indicators.go
@@ -11,9 +11,9 @@ type RateOfChange struct {
 	RightVal float64
 }
 
-func (roc *RateOfChange) Update(closePrice float64) {
-	roc.RightVal = closePrice
-	roc.Values.PushRight(closePrice)
+func (roc *RateOfChange) Update(newVal float64) {
+	roc.RightVal = newVal
+	roc.Values.PushRight(newVal)
 	if roc.Values.Size() > roc.MaxLen {
 		roc.Values.PopLeft()
 	}
@@ -22,4 +22,23 @@ func (roc *RateOfChange) Update(closePrice float64) {
 
 func (roc *RateOfChange) GetVal() float64 {
 	return (roc.RightVal - roc.LeftVal) / roc.LeftVal
+}
+
+type SMA struct {
+	Values *deque.Deque
+	MaxLen float64
+	CurSum float64
+}
+
+func (sma *SMA) Update(newVal float64) {
+	sma.Values.PushRight(newVal)
+	sma.CurSum += newVal
+	if sma.Values.Size() > sma.MaxLen {
+		removedVal := float64(sma.Values.PopLeft().(float64))
+		sma.CurSum -= newVal
+	}
+}
+
+func (sma *SMA) GetVal() float64 {
+	return sma.CurSum / sma.MaxLen
 }
