fixed indicator calculation bug

beverly_hills/src/indicators/cci.py
@@ -18,7 +18,7 @@ class CCI(Indicator):
         self.low_values.queue.append(data['low'])
         self.close_values.queue.append(data['close'])
         
-        if len(self.high_values.queue) == self.params['period']:
+        if len(self.high_values.queue) > self.params['period']:
             result = talib_CCI(np.asarray(self.high_values.queue, dtype=np.float64), np.asarray(self.low_values.queue, dtype=np.float64), np.asarray(self.close_values.queue, dtype=np.float64), timeperiod=self.params['period'])[-1]
             self.results.addData(result)
             scaled_result = 0.5

beverly_hills/src/indicators/rsi.py
@@ -17,7 +17,7 @@ class RSI(Indicator):
         self.values.queue.append(data[self.value])
         
         
-        if len(self.values.queue) == self.params['period']:
+        if len(self.values.queue) > self.params['period']:
             result = talib_RSI(np.asarray(self.values.queue, dtype=np.float64), timeperiod=self.params['period'])[-1]
             self.results.addData(result)
             scaled_result = 0.5

beverly_hills/src/indicators/willr.py
@@ -17,7 +17,7 @@ class WILLR(Indicator):
         self.low_values.queue.append(data['low'])
         self.close_values.queue.append(data['close'])
         
-        if len(self.high_values.queue) == self.params['period']:
+        if len(self.high_values.queue) > self.params['period']:
             result = talib_WILLR(np.asarray(self.high_values.queue, dtype=np.float64), np.asarray(self.low_values.queue, dtype=np.float64), np.asarray(self.close_values.queue, dtype=np.float64), timeperiod=self.params['period'])[-1]
             self.results.addData(result)
             scaled_result = 0.5