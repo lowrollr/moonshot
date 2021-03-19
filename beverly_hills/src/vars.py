dd6a452f3919a980e738f316428e4b88df97856b
marshingjay <jam605@gmail.com>
2021-03-18 22:14:58 -0500

    commented vars.py

--- beverly_hills/src/vars.py
+++ beverly_hills/src/vars.py
@@ -1,4 +1,12 @@
+'''
+FILE: vars.py
+AUTHORS:
+    -> Ross Copeland (rhcopeland101@gmail.com)
+WHAT:
+    -> holds vars mapping containers to id and vice versa
+'''
 
+# map container names to internal ids
 containersToId = {
     "main_data_consumer":0,
     "beverly_hills":1,
@@ -6,6 +14,7 @@ containersToId = {
     "frontend":3
 }
 
+# map internal ids to container names
 idToContainer = {
     0:"main_data_consumer",
     1:"beverly_hills",
